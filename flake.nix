5d5f09ef49a32e5043b123ef39eddbf5c5405cb2
DaKingof <31186364+DaKingof@users.noreply.github.com>
2025-10-25 15:33:30 -0400

    Add OPENSSL_DIR to environment variables

--- flake.nix
+++ flake.nix
@@ -97,6 +97,7 @@
           ];
           env =
             {
+              OPENSSL_DIR = "${pkgs.openssl.dev}";
               SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
               UV_PYTHON_DOWNLOADS = "never";
               UV_PYTHON = python.interpreter;
