dd88bd37dee8006928fc4982964d8691d37cabbb
Dominik Jain <dominik.jain@oraios-ai.de>
2026-03-05 20:57:54 +0100

    Mark TestFSharpLanguageServer.test_go_to_definition with xfail

--- test/solidlsp/fsharp/test_fsharp_basic.py
+++ test/solidlsp/fsharp/test_fsharp_basic.py
@@ -109,6 +109,7 @@ class TestFSharpLanguageServer:
         # The subtract function should be referenced in Program.fs
         assert any("Program.fs" in ref.get("relativePath", "") for ref in refs), "Program.fs should reference subtract function"
 
+    @pytest.mark.xfail(is_ci, reason="Test is flaky")  # TODO: Re-enable if the LS can be made more reliable #1040
     @pytest.mark.parametrize("language_server", [Language.FSHARP], indirect=True)
     def test_go_to_definition(self, language_server: SolidLanguageServer) -> None:
         """Test go-to-definition functionality."""
