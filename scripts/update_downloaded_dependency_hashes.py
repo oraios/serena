5584e75a18a4ed9c669bc5c7afd8ff430f3c8c82
Michael Panchenko <michael.panchenko@oraios-ai.de>
2026-08-08 19:42:38 +0200

    formating

--- scripts/update_downloaded_dependency_hashes.py
+++ scripts/update_downloaded_dependency_hashes.py
@@ -1,8 +1,8 @@
 from sensai.util import logging
 
 from solidlsp.language_servers.eclipse_jdtls import EclipseJDTLS
-from solidlsp.language_servers.nextflow_language_server import NextflowLanguageServer
 from solidlsp.language_servers.kotlin_language_server import KotlinLanguageServer
+from solidlsp.language_servers.nextflow_language_server import NextflowLanguageServer
 
 if __name__ == "__main__":
     logging.configure()
